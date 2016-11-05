edff411839c3e3095a996dae863f53d5bf443606
Reelix <reelix@gmail.com>
2016-11-05 14:14:17 +0200

    Added StringExtensions.cs with the IsAPangram method

--- Program.cs
+++ Program.cs
@@ -8,10 +8,21 @@ namespace ReeCodeDemo
     {
         static void Main(string[] args)
         {
+            // ListExtensions.cs
+            // Median
             List<double> medianList = new List<double>() { 1, 2, 3, 4, 5 };
-            List<int> factorsList = new List<int>() { 2, 5, 10 };
             Console.WriteLine(medianList.Median()); // 3
+
+            // AreAlLFactorsOf
+            List<int> factorsList = new List<int>() { 2, 5, 10 };
             Console.WriteLine(factorsList.AreAllFactorsOf(20)); // True
+
+            // StringExtensions.cs
+            // IsAPangram
+            string notAPangram = "This is not a pangram";
+            string isAPangram = "The quick brown fox jumps over the lazy dog";
+            Console.WriteLine(notAPangram.IsAPangram()); // False
+            Console.WriteLine(isAPangram.IsAPangram()); // True
             Console.ReadLine();
         }
     }
---
+++ StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ReeCode
+{
+    public static class StringExtensions
+    {
+        public static bool IsAPangram(this string inputString)
+        {
+            inputString = inputString.ToLower();
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+                if (!inputString.Contains(c.ToString()))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
