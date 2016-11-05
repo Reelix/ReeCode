507b5ec485a98ffaa991e44b6b9944c2ed582726
Reelix <reelix@gmail.com>
2016-11-05 13:36:30 +0200

    Some basic List<> extension methods

---
+++ ReeCode/ListExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReeCode
+{
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// Finds the Median value of a list
+        /// </summary>
+        /// <param name="inputList"></param>
+        /// <returns>The Median value</returns>
+        public static double Median(this List<double> inputList)
+        {
+            double[] xs = inputList.ToArray();
+            Array.Sort(xs);
+            return xs[xs.Length / 2];
+        }
+
+        /// <summary>
+        /// Checks if all values in the list are factors of a number
+        /// </summary>
+        /// <param name="inputList"></param>
+        /// <param name="x"></param>
+        /// <returns>True / False</returns>
+        public static bool AreAllFactorsOf(this List<int> inputList, int x)
+        {
+            foreach (int num in inputList)
+            {
+                if (x % num != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ ReeCode/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReeCode;
+
+namespace ReeCodeDemo
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            List<double> medianList = new List<double>() { 1, 2, 3, 4, 5 };
+            List<int> factorsList = new List<int>() { 2, 5, 10 };
+            Console.WriteLine(medianList.Median()); // 3
+            Console.WriteLine(factorsList.AreAllFactorsOf(20)); // True
+            Console.ReadLine();
+        }
+    }
+}
