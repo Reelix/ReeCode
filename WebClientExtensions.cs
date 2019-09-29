80f0a58fcffd7b09a8e674eefd19aef7c031b629
Reelix <reelix@gmail.com>
2019-09-29 04:00:04 +0200

    - Added Json support to .Post

--- WebClientExtensions.cs
+++ WebClientExtensions.cs
@@ -50,16 +50,45 @@ namespace ReeCode
         /// <summary>
         /// Makes a WebClient POST request, and returns the result as a string
         /// </summary>
-        public static string Post(this WebClient theWebClient, string URL, Dictionary<string, string> postValues)
+        public static string Post(this WebClient theWebClient, string URL, Dictionary<string, string> postValues, bool isJSON = false)
         {
             NameValueCollection postCollection = new NameValueCollection();
             foreach (var item in postValues)
             {
                 postCollection.Add(item.Key, item.Value);
             }
-            byte[] responseBytes = theWebClient.UploadValues(URL, "POST", postCollection);
-            string responseString = Encoding.UTF8.GetString(responseBytes);
-            return responseString;
+            if (isJSON)
+            {
+                theWebClient.SetContentType("application/json");
+                var jsonData = postCollection.AllKeys.ToDictionary(x => x, x => postCollection[x]);
+                // using System.Web.Script.Serialization;
+                var json = new JavaScriptSerializer().Serialize(jsonData);
+                try
+                {
+                    string response = theWebClient.UploadString(URL, json);
+                    return response;
+                }
+                catch (WebException wex)
+                {
+                    if (((HttpWebResponse)wex.Response).StatusCode == HttpStatusCode.NotFound)
+                    {
+                        StreamReader sr1 = new StreamReader(wex.Response.GetResponseStream());
+                        string theLine = sr1.ReadToEnd();
+                        return theLine;
+                    }
+                    return "Error - " + wex.Message;
+                }
+                catch (Exception ex)
+                {
+                    return "Error - " + ex.Message;
+                }
+            }
+            else
+            {
+                byte[] responseBytes = theWebClient.UploadValues(URL, "POST", postCollection);
+                string responseString = Encoding.UTF8.GetString(responseBytes);
+                return responseString;
+            }
         }
     }
 }
